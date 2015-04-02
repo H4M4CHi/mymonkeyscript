39459f314fa0f54afabf342f7f78ba8bff05f6ba
Hamada Yuma <d.hama.dev@gmail.com>
2015-04-02 23:58:47 +0900

    Create improve_dam_jslink.user.js

---
+++ improve_dam_jslink.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         My Fancy New Userscript
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @match        http://www.clubdam.com/*
+// @grant        none
+
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+$("li[onclick^='location']").each(function(idx, e) {
+	li = $(e);
+	url = li.attr("onclick").split("'")[1];
+	li.wrap("<a href='" + url + "'></a>");
+    li.attr("onclick", "");
+});
